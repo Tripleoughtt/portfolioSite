dc783a8c98747503a5c47f1e8df69a258111251c
Richard Gautereaux <ReliX@mothership-zeta.attlocal.net>
2016-01-14 19:25:18 -0800

    Working Gulpfile Build

---
+++ README.md
@@ -0,0 +1,6 @@
+# frontendReactTemplate
+
+Step 1: To begin run npm i && jspm i 
+
+Step 2: run "gulp" to host your server, watch your files and bundle jspm along with your 
+packages
---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+var gulp_jspm = require('gulp-jspm');
+var addsrc = require('gulp-add-src');
+var rimraf = require('rimraf');
+var sass = require('gulp-sass')
+
+gulp.task('serve', function(cb){  
+  browserSync.init({
+    server: "./"
+  })
+
+  gulp.watch('./build/styles/**/*').on('change', browserSync.reload)
+  gulp.watch('./**/*.html').on('change', browserSync.reload)
+  gulp.watch('./build/main.bundle.js').on('change', browserSync.reload)
+});
+
+gulp.task('sass', ["clean", 'posts'], function(){
+  gulp.src('./sass/**/*.sass')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./build/css'))
+});
+gulp.task('posts', function(){
+  gulp.src('./posts')
+    .pipe(gulp.dest('./build/posts'))
+})
+
+
+gulp.task('jspm-build', ['sass'], function(cb){
+  return gulp.src('lib/main.js')
+    .pipe(gulp_jspm({selfExecutingBundle : true}))
+    .pipe(gulp.dest('build/', cb))
+});
+
+gulp.task('clean', function(cb){
+  rimraf('/build', cb)
+});
+
+gulp.task('watch', function() {
+  gulp.watch('lib/**/*', ['jspm-build'])
+  gulp.watch('sass/**/*', ['jspm-build'])
+});
+
+gulp.task('default', ['jspm-build', 'serve','watch'])
